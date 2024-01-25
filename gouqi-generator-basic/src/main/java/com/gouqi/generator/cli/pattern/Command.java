3937b66906bfc9f3e510ec6c87de6ae2241e5f25
zhJQ1218 <977274456@qq.com>
2024-01-26 00:48:42 +0800

    编写命令模式样例

---
+++ gouqi-generator-basic/src/main/java/com/gouqi/generator/cli/pattern/Command.java
@@ -0,0 +1,5 @@
+package com.gouqi.generator.cli.pattern;
+
+public interface Command {
+    void execute();
+}
---
+++ gouqi-generator-basic/src/main/java/com/gouqi/generator/cli/pattern/TurnOffCommand.java
@@ -0,0 +1,14 @@
+package com.gouqi.generator.cli.pattern;
+
+public class TurnOffCommand implements Command {
+
+    private Device device;
+
+    public TurnOffCommand(Device device) {
+        this.device = device;
+    }
+
+    public void execute() {
+        device.turnOff();
+    }
+}
