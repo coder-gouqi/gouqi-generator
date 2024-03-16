35deddb056d151abb477816232492c2c58f4a9f5
zhJQ1218 <977274456@qq.com>
2024-03-17 00:45:34 +0800

    优化下载生成器接口，使用本地缓存

---
+++ gouqi-generator-web-backend/src/main/java/com/gouqi/model/dto/generator/GeneratorCacheRequest.java
@@ -0,0 +1,20 @@
+package com.gouqi.model.dto.generator;
+
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.Map;
+
+/**
+ * 缓存生成器请求
+ */
+@Data
+public class GeneratorCacheRequest implements Serializable {
+
+    /**
+     * 生成器的 id
+     */
+    private Long id;
+
+    private static final long serialVersionUID = 1L;
+}
